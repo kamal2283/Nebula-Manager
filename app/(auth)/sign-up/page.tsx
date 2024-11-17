47ac43941da08d10eb99f0594f579743425dac6a
Kamal Shah <shahkamal2283@gmail.com>
2024-11-17 19:22:31 +0530

    add authentication forms and validation; implement CustomInput component

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import AuthForm from "@/components/AuthForm";
+import React from "react";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  );
+};
 
-export default SignUp
-
-// 30:14
+export default SignUp;
--- lib/utils.ts
+++ lib/utils.ts
@@ -212,3 +212,9 @@ export const authFormSchema = (type: string) =>
     email: z.string().email(),
     password: z.string().min(8),
   });
+
+export const authformSchema = z.object({
+  email: z.string().email(),
+  username: z.string().min(3),
+  password: z.string().min(8),
+});

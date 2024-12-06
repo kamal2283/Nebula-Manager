d3fbd7fc4352b87341dc1850c8ced6029f8e4fd1
Kamal Shah <shahkamal2283@gmail.com>
2024-12-07 01:11:42 +0530

    add Appwrite integration; update environment variables, enhance user authentication, and implement footer component

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,19 +4,23 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetTrigger,
+  SheetDescription,
+  SheetHeader,
   SheetTitle,
+  SheetTrigger,
 } from "@/components/ui/sheet";
 import { sidebarLinks } from "@/constants";
-import Link from "next/link";
-import Image from "next/image";
-import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
+
   return (
-    <section className="w-full mx-w-[264px]">
+    <section className="w-fulll max-w-[264px]">
       <Sheet>
         <SheetTrigger>
           <Image
@@ -28,32 +32,33 @@ const MobileNav = ({ user }: MobileNavProps) => {
           />
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-white">
-          <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
-          <nav className="flex flex-col h-full">
-            <Link
-              href="/"
-              className="cursor-pointer flex items-center gap-1 px-4"
-            >
-              <Image
-                src="/icons/logo.svg"
-                width={34}
-                height={34}
-                alt="Nebula Logo"
-              />
-              <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
-                Nebula
-              </h1>
-            </Link>
-            <div className="mobilenav-sheet">
-              <div className="flex flex-col gap-6 pt-16 text-white">
+          <Link
+            href="/"
+            className="cursor-pointer flex items-center gap-1 px-4"
+          >
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Horizon logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              Horizon
+            </h1>
+          </Link>
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
                 {sidebarLinks.map((item) => {
                   const isActive =
                     pathname === item.route ||
                     pathname.startsWith(`${item.route}/`);
+
                   return (
                     <SheetClose asChild key={item.route}>
                       <Link
                         href={item.route}
+                        key={item.label}
                         className={cn("mobilenav-sheet_close w-full", {
                           "bg-bank-gradient": isActive,
                         })}
@@ -78,9 +83,12 @@ const MobileNav = ({ user }: MobileNavProps) => {
                     </SheetClose>
                   );
                 })}
-              </div>
-            </div>
-          </nav>
+                USER
+              </nav>
+            </SheetClose>
+
+            <Footer user={user} type="mobile" />
+          </div>
         </SheetContent>
       </Sheet>
     </section>
